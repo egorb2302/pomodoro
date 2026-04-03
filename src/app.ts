012edca7f82a9066bb95f5dae8cfc4f7e4352849
egorb2302 <zxcgovnoedig@gmail.com>
2026-04-03 18:48:55 +0400

    complete counter ?v1

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
+import './style.css';
+
 document.body.innerHTML = 
 `
     <div class="pomodoro_cont">
+      <img class="pomodoro-pic" src="../src/assets/pomodoro.png" alt="">
       <div class="pomodoro-content">
         <h1 class="pomodoro">Pomodoro</h1>
         <p class="pomodoro-timer"><span class="minutes">00</span>
